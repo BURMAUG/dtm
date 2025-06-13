97bfd670881d91e3974f78c651be4f1e6d0a2493
Burmau Garba <burmaud1@gmail.com>
2025-06-13 13:26:25 -0400

    Setup of email is up and running

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"net/http"
 
+	_ "github.com/dtm/internal/config"
 	"github.com/dtm/internal/delivery"
 )
 
 func main() {
 	http.HandleFunc("/landing", delivery.Landing)
-	http.HandleFunc("/form", delivery.Form)
+	http.HandleFunc("/form", delivery.ServeForm)
+	http.HandleFunc("/submit", delivery.Form)
 	http.HandleFunc("/reserve", delivery.CustomerReservation)
 
 	http.ListenAndServe(":8080", nil)
--- internal/repository/reservation-repository.go
+++ internal/repository/reservation-repository.go
@@ -2,29 +2,32 @@ package repository
 
 import (
 	"context"
+	"log"
 
 	"github.com/dtm/internal/config"
 	"github.com/dtm/internal/domain"
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
-var db *gorm.DB = config.Conn()
+var db = config.Conn()
 
 type Customer interface {
 	FindCustomerById(ctx context.Context, id uuid.UUID)
 	SaveCustomerInfo(ctx context.Context, customerInfo *domain.CustomerInfo) error
 }
 
-type CustomerData struct{}
+type CustomerInfo struct{}
 
-func (c *CustomerData) FindCustomerById(ctx context.Context, id uuid.UUID) {
+func (c *CustomerInfo) FindCustomerById(ctx context.Context, id uuid.UUID) {
 	db.WithContext(ctx).First(&domain.CustomerInfo{}, "customer_id = ? ", id)
 }
 
-func (c *CustomerData) SaveCustomerInfo(ctx context.Context, customerInfo *domain.CustomerInfo) error {
-	if err := db.WithContext(ctx).Save(customerInfo).Error; err != nil {
-		db.AddError(err)
+func (c *CustomerInfo) SaveCustomerInfo(ctx context.Context, customerInfo *domain.CustomerInfo) error {
+	log.Print("@@@@@@@@@@@@@@@@@@@Customer", customerInfo)
+	log.Print("@@@@@@@@@@DB", db)
+
+	if err := db.WithContext(ctx).Create(customerInfo).Error; err != nil {
+		log.Print("DB Errorr @@@@@@@ ", err)
 		return err
 	}
 	return nil

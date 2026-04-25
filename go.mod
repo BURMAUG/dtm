badacc03081ab919b5bd4cf40ccc53053080dd56
Burm0 <burm0@pop-os.lan>
2026-04-25 15:25:59 -0400

    All new change

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.24.3
 
 require (
 	github.com/google/uuid v1.6.0
-	gorm.io/driver/postgres v1.6.0
-	gorm.io/gorm v1.30.0
+//	gorm.io/driver/postgres v1.6.0
+//	gorm.io/gorm v1.30.0
 )
+
+require github.com/BURMAUG/dtm-common v0.0.4 // indirect
 
 require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -19,5 +21,5 @@ require (
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
--- internal/delivery/reservation-delivery.go
+++ internal/delivery/reservation-delivery.go
@@ -1,79 +1,23 @@
 package delivery
 
 import (
-	"html/template"
 	"log"
 	"net/http"
-
-	"github.com/dtm/internal/usecase"
 )
-
-const (
-	indexHTML       = "../template/index.gohtml"
-	reservationHTML = "../template/reservation.gohtml"
-	formHTML        = "../template/form.gohtml"
-	thanksHTML      = "../template/thanks.gohtml"
-)
-
-var customerReservation = usecase.CustomerReservationUsecase{}
 
 func Landing(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL.Path)
-
-	tmpl, err := template.ParseFiles(indexHTML)
-	if r.Method != http.MethodGet || err != nil {
-		log.Print(err)
-		http.Error(w, "Sorry", http.StatusInternalServerError)
-		return
-	}
-	tmpl.Execute(w, nil)
 }
 
 // serve the form /form
 func ServeForm(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
-
-	tmpl, err := template.ParseFiles(formHTML)
-	if r.Method != http.MethodGet || err != nil {
-		log.Print(r.Method)
-		log.Print("form error ", err)
-		http.Error(w, "Sorry", http.StatusInternalServerError)
-		return
-	}
-
-	tmpl.Execute(w, nil)
-	// talk to usecase layer
 }
 
 // submit the form with data /reserve
 func Form(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL.Path)
-	if r.Method != http.MethodPost {
-		log.Print(r.Method)
-		http.Error(w, "Sorry", http.StatusInternalServerError)
-		return
-	}
-
-	// talk to usecase layer
-	customerReservation.MakeReservation(r.Context(), r)
-	tmpl, err := template.ParseFiles(thanksHTML)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	tmpl.Execute(w, nil)
 }
 
 // get and displays customer information based on the id being sent.
 func CustomerReservation(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL.Path)
-	tmpl, err := template.ParseFiles(reservationHTML)
-	if r.Method != http.MethodGet || err != nil {
-		http.Error(w, "Sorry", http.StatusInternalServerError)
-		return
-	}
 
-	customerReservation.GetCustomerReservation(r.Context(), r)
-	tmpl.Execute(w, nil)
-	// talk to usecase layer
 }
